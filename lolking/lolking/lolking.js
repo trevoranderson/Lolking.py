be3a5e49f88ecf1402a9f5223d587a14098b2f70
trevoranderson <trevora@ucla.edu>
2015-02-18 01:16:03 -0800

    python is annoying ported to node

---
+++ lolking/lolking/lolking.js
@@ -0,0 +1,19 @@
+﻿var request = require('request');
+var _ = require('lodash');
+var summonerName = "a single hat";
+var idgetter = "http://www.lolking.net/search?name=" + summonerName.split(" ").join("") + "&region=na";
+request.get(idgetter, function (e, r) {
+    var sid = r.request.href.split("/").pop();
+    var matchgetter = "http://www.lolking.net/summoner/na/" + sid + "#matches";
+    request.get(matchgetter, function (e, r, b) {
+        var history = JSON.parse(b.split("var history = ")[1].split(";\n")[0]);
+        var names = _.flatten(_.map(history,function (o) { return o.match.teammates.concat(o.match.opponents.concat([o.match.summoner])); }).map(function (o) { return o.map(function (o) { return o.name; }); }));
+        var counts = _.countBy(names);
+        _.forOwn(counts, function (v, k) {
+            if (v > 1) {
+                console.log(k + " : " + v);
+            }
+        });
+        while (1) { }
+    });
+});
